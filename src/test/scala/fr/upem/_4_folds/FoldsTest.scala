edbd6cb3c5d9c228e89b0df8dcd9580b32b80402
Ilja <ilja.kempf@gmail.com>
2018-12-10 12:37:03 +0100

    Test acc side in foldable test

--- src/test/scala/fr/upem/_4_folds/FoldsTest.scala
+++ src/test/scala/fr/upem/_4_folds/FoldsTest.scala
@@ -1,30 +1,28 @@
 package fr.upem._4_folds
 
-import cats.{Eval, Foldable, Traverse}
+import cats.implicits._
+import cats.laws.discipline.FoldableTests
+import cats.{Eval, Foldable}
+import fr.upem.CheckLaws
 import fr.upem._3_functor.Tree
 import fr.upem._3_functor.Tree.{Leaf, Node}
+import fr.upem._4_folds.TreeFolds._
 import org.scalatest.{FlatSpec, Matchers}
-import TreeFolds._
-import cats.implicits._
-import cats.laws.discipline.{FoldableTests, TraverseTests}
-import fr.upem.CheckLaws
 
 class FoldsTest extends FlatSpec with Matchers with CheckLaws {
 
   "Foldable" should "fold tree right side first" in {
     val tree = Node("b", Node("d", Leaf, Node("c", Leaf, Leaf)), Node("a", Leaf, Leaf))
 
-    Foldable[Tree].foldRight(tree, Eval.now(""))((a, acc) => acc.map(a + _)).value should equal("abcd")
+    Foldable[Tree].foldRight(tree, Eval.now("z"))((a, acc) => acc.map(a + _)).value should equal("abcdz")
   }
 
   it should "fold tree left side first" in {
-    val tree = Node("c", Node("a", Leaf, Node("b", Leaf, Leaf)), Node("d", Leaf, Leaf))
+    val tree = Node("b", Node("d", Leaf, Node("c", Leaf, Leaf)), Node("a", Leaf, Leaf))
 
-    Foldable[Tree].foldLeft(tree, "")(_ + _) should equal("abcd")
+    Foldable[Tree].foldLeft(tree, "z")(_ + _) should equal("zabcd")
   }
 
   checkAll("Foldable[Tree]", FoldableTests[Tree].foldable[Int, Int])
-
-  val isTwo: Int => Option[Int] = x =>  if (x == 2) Some(2) else None
 
 }
